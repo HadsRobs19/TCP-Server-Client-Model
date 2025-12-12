ed2ef097f8db30e9610f2844960845aa982c74e5
Hadassah Roberts <118022490+HadsRobs19@users.noreply.github.com>
2025-12-12 12:34:32 -0500

    additions to tcp server

---
+++ core/tcp-server.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int net_server() {
+
+    unsigned short port = 8080;
+
+    // local memory array used to temporarily hold data as it moves between application layer and the kernels internal TCP socket buffer
+    char buffer[1024] = { 0 };
+
+    //stores address for the Internet addr family
+    struct sockaddr_in addr;
+    socklen_t addr_l = sizeof(addr);
+
+    // binding socket addr to a port (8080)
+    addr.sin_family = AF_INET; //IPv4 
+    addr.sin_port = htons(port); // 8080 local port
+    addr.sin_addr.s_addr = INADDR_ANY; // any local IPv4 addr
+
+
+    // creating of a server socket and check if it is created
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd < 0){
+        perror("socket not found");
+        exit(EXIT_FAILURE);
+    }
+
+    // helps manipulate options for fd; prevents "address already in use" errors
+    int option = 1;
+
+    if(setsockopt(fd, IPPROTO_TCP, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof(option))){
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // binds a name to my socket fd (socket, points to sockaddr structure containing the address to the bound socket, specifies the length of the sockaddr structure pointed to by the address argument)
+    if(!(bind(fd, (struct sockaddr_in*)&addr, addr_l))) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // listen for socket connections and limit the queue of incoming connections
+    // socket and backlog para -> backlog describes the limit for the queue
+
+    // maximum backlog -> 128 (look up backlog more0)
+    // backlog: number representing the size of the queue holding the pending connections while the server is waiting to accept a connection.
+    if(!listen(fd, 2)){
+        perror("listen failure");
+        exit(EXIT_FAILURE);
+    }
+    
+    // accept extracts the first connection on the queue of pending connections, creates a new socket with the same socket type protocol and address family as the specificied socket, and allocates a new file descriptor for that socket
+    int new_fd = accept(fd, (struct sockaddr_in*)&addr, &addr_l);
+    if(!new_fd){
+        perror("accept failure");
+        exit(EXIT_FAILURE);
+    }
+    
+    // ssize_t -> signed int type representing a count of bytes and returns -1
+    // read attempts to read byte data from fd socket into the buffer queue
+    ssize_t read_data = read(fd, buffer, 1024 - 1);
+
+       
+    return 0;
+}
